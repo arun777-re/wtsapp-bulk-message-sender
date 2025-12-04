56e10394d06f63859a13e767c366aee3c2877715
Arun <arunjamdagni2@gmail.com>
2025-12-04 22:55:26 +0530

    style: button styling

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -8,14 +8,15 @@ const Main = () => {
  
   return (
     <main className="w-full min-h-[calc(100vh-60px)] flex items-center justify-center bg-bgSoft px-4 flex-col gap-8 ">
-      <div className="max-w-2xl w-full bg-white shadow-md rounded-2xl p-10 border border-gray-200">
+      <div className="max-w-2xl w-full bg-white shadow-md rounded-2xl p-10 border border-gray-200
+      flex flex-col items-center justify-center gap-4">
         <UploadForm onUpload={uploadDataFile} loading={loading} />
         <Link
           href={"/send-msg"}
-          className="px-10 py-4 border-main bg-main cursor-pointer text-text rounded-sm transition-colors duration-300
-           hover:bg-secondary hover:text-white"
+          className="px-10 py-2 border-main bg-button cursor-pointer text-white font-semibold rounded-lg shadow-sm transition-all duration-300
+           hover:bg-main leading-loose hover:text-white/80 hover:shadow-md"
         >
-          View
+          send message
         </Link>
       </div>
     </main>
