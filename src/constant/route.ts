fe7ba2eb7826c35c362371b9c24c4ea190ec9086
Arun <arunjamdagni2@gmail.com>
2025-12-04 22:00:36 +0530

    refactor: env

--- src/constant/route.ts
+++ src/constant/route.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_BASE_URL || "http://localhost:8000";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8000";
 
 export const ROUTES = {
   WHATSAPPURL:"whatsapp://send?phone=",
