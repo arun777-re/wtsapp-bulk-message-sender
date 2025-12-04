7d33ba869f7f47d39d481e29397f59dc87932706
Arun <arunjamdagni2@gmail.com>
2025-12-04 22:21:04 +0530

    feat: add delete data button

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,25 +1,21 @@
-'use client'
-import React from "react";
+"use client";
 import UploadForm from "./UploadForm";
 import useCSVHook from "@/hooks/useCSVHook";
 import Link from "next/link";
 
 const Main = () => {
-    const [isTrue,setIsTrue] = React.useState<boolean>(false)
-    const {data,uploadDataFile,loading} = useCSVHook();
-    const handleTrue = (e:React.MouseEvent<HTMLButtonElement>)=>{
-e.preventDefault();
-e.stopPropagation();
-setIsTrue((p)=>!p)
-alert(isTrue)
-    }
+  const {  uploadDataFile, loading } = useCSVHook();
+ 
   return (
     <main className="w-full min-h-[calc(100vh-60px)] flex items-center justify-center bg-bgSoft px-4 flex-col gap-8 ">
       <div className="max-w-2xl w-full bg-white shadow-md rounded-2xl p-10 border border-gray-200">
-        <UploadForm onUpload={uploadDataFile} loading={loading}/>
-           <Link href={'/send-msg'} className="px-10 py-4 border-main bg-main cursor-pointer text-text rounded-sm "
-      >
-           View
+        <UploadForm onUpload={uploadDataFile} loading={loading} />
+        <Link
+          href={"/send-msg"}
+          className="px-10 py-4 border-main bg-main cursor-pointer text-text rounded-sm transition-colors duration-300
+           hover:bg-secondary hover:text-white"
+        >
+          View
         </Link>
       </div>
     </main>
--- src/app/send-msg/page.tsx
+++ src/app/send-msg/page.tsx
@@ -4,17 +4,28 @@ import SingleMsgForm from "../components/SingleMsgForm";
 import BulkMessage from "../components/BulkMessage";
 
 const Page = () => {
-const [data,setData] = React.useState<any[]>([])
-        React.useEffect(()=>{
-      const raw = localStorage.getItem("data");
-      if(raw) setData(JSON.parse(raw));
-        },[]);
-     console.log("data in main",data)
+  const [data, setData] = React.useState<any[]>([]);
+  React.useEffect(() => {
+    const raw = localStorage.getItem("data");
+    if (raw) setData(JSON.parse(raw));
+  }, []);
+
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    localStorage.removeItem("data");
+  };
   return (
     <div className="w-full min-h-screen bg-gray-50 px-4 py-10">
       <div className="max-w-4xl mx-auto flex flex-col gap-6">
-    <BulkMessage data={data}/>
-
+        <BulkMessage data={data} />
+        <button
+          onClick={handleDelete}
+          className="bg-red-400 text-black rounded-md px-4 py-4 cursor-pointer transition-colors
+          hover:bg-red-400/90 hover:text-text"
+        >
+          Delete Data
+        </button>
         {/* Data List */}
         <div className="flex flex-col gap-4">
           {data &&
@@ -42,8 +53,7 @@ const [data,setData] = React.useState<any[]>([])
                 >
                   Call Now
                 </a>
-               <SingleMsgForm phoneno={value.rawphone}/>
-             
+                <SingleMsgForm phoneno={value.rawphone} />
               </div>
             ))}
         </div>
