45f1014bb221bee6e89aec7150b3e6b71a46bffe
Arun <arunjamdagni2@gmail.com>
2025-12-05 00:11:48 +0530

    feat: add react-hot-toast for notify

--- src/app/components/BulkMessage.tsx
+++ src/app/components/BulkMessage.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import toast from "react-hot-toast";
 import useCSVHook from "@/hooks/useCSVHook";
 import Spinner from "./Spinner";
 
@@ -8,53 +9,60 @@ type Props = {
 };
 
 const BulkMessage: React.FC<Props> = ({ data }) => {
-
-    
-  const [message, setMessage] = React.useState<string>("");
-  const [loading, setLoading] = React.useState<boolean>(false);
+  const [message, setMessage] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
   const { sendBulkMessage } = useCSVHook();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!message.trim()) return alert("Message cannot be blank!");
-    if (!data || data.length === 0) return alert("No contacts uploaded!");
-console.log("data",data);
+    if (!message.trim()) return toast.error("Message cannot be blank!");
+    if (!data || data.length === 0) return toast.error("No contacts uploaded!");
+
     try {
       setLoading(true);
       await sendBulkMessage({ message, phonenos: data });
-      alert("Messages successfully sent!");
+
+      toast.success("Messages sent successfully!");
+      setMessage("");
     } catch (err) {
-      alert("Failed to send messages!");
       console.error(err);
+      toast.error("Failed to send messages!");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-      <label className="text-lg font-semibold text-gray-700">
-        Enter Message
-      </label>
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <label className="text-lg font-semibold text-gray-700">Enter Message</label>
 
       <input
         type="text"
         placeholder="Type message..."
         value={message}
+        disabled={loading}
         onChange={(e) => setMessage(e.target.value)}
         className="px-4 py-3 rounded-lg border border-gray-300 outline-none 
+        disabled:bg-gray-100 disabled:cursor-not-allowed 
         focus:border-primary focus:ring-2 focus:ring-primary/40 transition"
       />
 
       <button
         type="submit"
         disabled={loading}
-        className="px-10 py-4 rounded-md bg-main border-secondary text-white 
+        className="px-10 py-3 rounded-md bg-main border-secondary text-white 
+        font-semibold shadow hover:shadow-md transition 
         disabled:opacity-50 disabled:cursor-not-allowed"
       >
-        {loading ? <Spinner/> : "Send Message to All"}
+        {loading ? (
+          <div className="flex items-center justify-center gap-2">
+            <Spinner /> Sending…
+          </div>
+        ) : (
+          "Send Message to All"
+        )}
       </button>
     </form>
   );
